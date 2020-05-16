1f6473b3cf4941f44eb41eb45669c2bc0ad21703
HungNhOnPV <anhhungx9propro@gmail.com>
2020-05-16 09:12:19 +0700

    config run webpack 4.0

--- lesson01-demo/webpack.config.js
+++ lesson01-demo/webpack.config.js
@@ -4,7 +4,8 @@ const config = {
   entry: './src/index.js',
   output: { 
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build')
+    path: path.resolve(__dirname, 'build'),
+    // watch: true
   }
 }
 
